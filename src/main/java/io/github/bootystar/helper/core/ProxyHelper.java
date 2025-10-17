c804154c621727371fd3b2391ad03e3f9336b377
bootystar <bootystar@qq.com>
2025-10-17 23:09:05 +0800

    注释未完成的类

--- src/main/java/io/github/bootystar/helper/core/ProxyHelper.java
+++ src/main/java/io/github/bootystar/helper/core/ProxyHelper.java
@@ -1,27 +1,27 @@
-package io.github.bootystar.helper.core;
-
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-
-/**
- * 代理助手
- * todo 待完善
- *
- * @author bootystar
- */
-public class ProxyHelper {
-    
-    
-    public static <T> T newProxyInstance(Class<T> clazz) {
-        return null;
-    }
-
-    
-    public static class Inner implements InvocationHandler{
-
-        @Override
-        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            return null;
-        }
-    }
-}
+//package io.github.bootystar.helper.core;
+//
+//import java.lang.reflect.InvocationHandler;
+//import java.lang.reflect.Method;
+//
+///**
+// * 代理助手
+// * todo 待完善
+// *
+// * @author bootystar
+// */
+//public class ProxyHelper {
+//    
+//    
+//    public static <T> T newProxyInstance(Class<T> clazz) {
+//        return null;
+//    }
+//
+//    
+//    public static class Inner implements InvocationHandler{
+//
+//        @Override
+//        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+//            return null;
+//        }
+//    }
+//}
